926b6aa7aa4d0f34d01896e629973ae206d85d2d
GBRKNIGHT <ethanli0417@gmail.com>
2022-11-13 00:23:44 -0500

    Nov 13 12AM

--- HW6/HW6/src/AuctionSystem.java
+++ HW6/HW6/src/AuctionSystem.java
@@ -28,6 +28,40 @@ public class AuctionSystem implements Serializable {
             }
             System.out.println("Please select a username: ");
             String userInputUsername = stdin.nextLine().trim();
+            AuctionSystem.userName = userInputUsername;
+            while (true){
+                System.out.println("(D) - Import Data from URL\n" +
+                        "(A) - Create a New Auction\n" +
+                        "(B) - Bid on an Item\n" +
+                        "(I) - Get Info on Auction\n" +
+                        "(P) - Print All Auctions\n" +
+                        "(R) - Remove Expired Auctions\n" +
+                        "(T) - Let Time Pass\n" +
+                        "(Q) - Quit\n" +
+                        "Please select an option:");
+                String userOption = stdin.nextLine().trim().toUpperCase();
+                switch (userOption){
+                    case "D":
+                        break;
+                    case "A":
+                        break;
+                    case "B":
+                        break;
+                    case "I":
+                        break;
+                    case "P":
+                        auctionTable.printTable();
+                        break;
+                    case "R":
+                        break;
+                    case "T":
+                        break;
+                    case "Q":
+                        System.exit(0);
+                }
+
+
+            }
 
         }catch (IllegalArgumentException IAE){
 
